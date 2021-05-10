d2028f575da2dd7afea2e8b2e863e3062a58560e
Alex Turpin <alex.turpin@gmail.com>
2021-05-09 21:21:43 -0400

    Styles first pass

--- pages/shop.tsx
+++ pages/shop.tsx
@@ -26,62 +26,90 @@ export default function Shop({ products }: ShopProps) {
     window.location = checkoutURL
   }
 
+  const product = products[0]
+
   return (
-    <div className="container mx-auto mt-10 shadow-md px-10 py-10 bg-white">
-      <div className="border-b pb-8">
-        <h1 className="font-semibold text-2xl">Shop</h1>
-      </div>
+    <>
+      <header className="w-full gradient">
+        <div className="container mx-auto">
+          <div className="absolute left-1/2 transform -translate-x-1/2">
+            <Image src="/eclipse-clipped.png" alt="" width="782" height="285" />
+          </div>
+          <h1 className="font-semibold text-base uppercase w-44 text-center mx-auto relative z-10">
+            Solar Eclipse Glasses
+          </h1>
+          <h3 className="font-semibold text-yellow text-center mb-14 relative z-10">
+            June 10, 2021
+          </h3>
 
-      <div className="flex mt-10 mb-5">
-        <h3 className="font-semibold text-gray-600 text-xs uppercase w-2/5">Product Details</h3>
-        <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">Price</h3>
-        <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">
-          Quantity
-        </h3>
-        <h3 className="font-semibold text-center text-gray-600 text-xs uppercase w-1/5">Total</h3>
-      </div>
+          <p className="text-center mb-6">
+            For those who have seen the Earth from space, and for the hundreds and perhaps thousands
+            more who will, the experience most certainly changes your perspective. The things that
+            we share in our world are far more valuable than those which divide us.
+          </p>
+          <p className="text-center mb-14">
+            It suddenly struck me that that tiny pea, pretty and blue, was the Earth. I put up my
+            thumb and shut one eye, and my thumb blotted out the planet Earth. I didn't feel like a
+            giant. I felt very, very small.
+          </p>
 
-      {products.map((product) => (
-        <div key={product.id} className="flex items-center hover:bg-gray-100 -mx-8 px-6 py-5">
-          <div className="flex w-2/5">
-            <div className="flex flex-col justify-between ml-4 flex-grow">
-              <div>
-                <Image layout="responsive" width="695" height="400" src={product.image} alt="" />
-              </div>
+          <h2 className="font-bold text-2xl text-yellow uppercase">Shop</h2>
+        </div>
+      </header>
+      <div className="container mx-auto">
+        <div className="bg-purple p-2">
+          <div className="flex flex-row justify-between items-center">
+            <div className="w-1/4">
+              <Image
+                layout="responsive"
+                width="1000"
+                height="1000"
+                src={product.image}
+                alt={product.title}
+              />
+            </div>
+            <div className="w-1/2">
               <span className="font-bold text-sm mb-5">{product.title}</span>
               <div
                 className="description-html text-sm"
                 dangerouslySetInnerHTML={{ __html: product.description }}
               ></div>
             </div>
+            <div className="">
+              <span className="text-center text-yellow font-semibold text-sm">
+                {formatCurrency(product.price)}
+              </span>
+            </div>
+            <div className="">
+              <input
+                className="mx-2 border text-center w-8"
+                type="number"
+                min="2"
+                value={quantity}
+                onChange={(e) => setQuantity(parseInt(e.target.value))}
+              />
+            </div>
           </div>
-          <span className="text-center w-1/5 font-semibold text-sm">
-            {formatCurrency(product.price)}
-          </span>
-          <div className="flex justify-center w-1/5">
-            <input
-              className="mx-2 border text-center w-8"
-              type="number"
-              min="2"
-              value={quantity}
-              onChange={(e) => setQuantity(parseInt(e.target.value))}
-            />
-          </div>
-          <span className="text-center w-1/5 font-semibold text-sm">
-            {formatCurrency(quantity * product.price)}
-          </span>
-        </div>
-      ))}
 
-      <div className="flex flex-row-reverse border-t pt-8">
-        <button
-          className="w-1/6 bg-indigo-500 font-semibold hover:bg-indigo-600 py-3 text-sm text-white uppercase"
-          onClick={checkout}
-          disabled={checkoutLoading}
-        >
-          Checkout
-        </button>
+          <div className="flex justify-between border-t mt-4 pt-2">
+            <span className="uppercase font-semibold">Subtotal</span>
+
+            <span className="text-yellow font-semibold">
+              {formatCurrency(quantity * product.price)}
+            </span>
+          </div>
+        </div>
+
+        <div className="flex flex-row-reverse">
+          <button
+            className="w-1/4 py-2 mt-4 rounded-lg text-sm text-white uppercase bg-gradient-to-r from-orange to-mangenta"
+            onClick={checkout}
+            disabled={checkoutLoading}
+          >
+            Checkout
+          </button>
+        </div>
       </div>
-    </div>
+    </>
   )
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,22 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    colors: {
+      white: "#FFFFFF",
+      yellow: "#FCB340",
+      orange: "#EC6E27",
+      mangenta: "#B93169",
+      purple: {
+        DEFAULT: "#291E3F",
+        dark: "#110C1E",
+      },
+    },
+    extend: {
+      zIndex: {
+        "-10": "-10",
+        "-20": "-20",
+      },
+    },
   },
   variants: {
     extend: {},
