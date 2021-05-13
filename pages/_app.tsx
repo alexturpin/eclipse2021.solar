c4f1cbb4977ae54aa237e83af483896721dfb625
Alex Turpin <alex.turpin@gmail.com>
2021-05-13 18:58:44 -0400

    Tweak Head and move to _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,37 @@
 import type { AppProps } from "next/app"
+import Head from "next/head"
 import "tailwindcss/tailwind.css"
 import "../styles/globals.css"
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+        <title>June 10, 2021 solar eclipse</title>
+
+        <link rel="preconnect" href="https://fonts.gstatic.com" />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;600;700;800&display=swap"
+          rel="stylesheet"
+        />
+
+        <meta
+          name="description"
+          content="Find out how you can see the eclipse. Visible from Eastern Canada!"
+        />
+
+        <meta property="og:title" content="June 10, 2021 solar eclipse" />
+        <meta
+          property="og:description"
+          content="Find out how you can see the eclipse. Visible from Eastern Canada!"
+        />
+        <meta property="og:image" content={process.env.NEXT_PUBLIC_DOMAIN + "/eclipse.png"} />
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN} />
+        <meta name="twitter:card" content="summary_large_image" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
 export default MyApp
