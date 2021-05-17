e83d5ce3028bc8a4bc2f9a168f3e202ecc541e37
Alex Turpin <alex.turpin@gmail.com>
2021-05-17 18:33:28 -0400

    Move shop to main page

--- next.config.js
+++ next.config.js
@@ -1,13 +1,4 @@
 module.exports = {
-  async redirects() {
-    return [
-      {
-        source: "/",
-        destination: "/shop",
-        permanent: false,
-      },
-    ]
-  },
   images: {
     domains: ["cdn.shopify.com"],
   },
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,24 @@
-export default function Home() {
-  return <p>Home</p>
+import { GetStaticProps } from "next"
+import { Header, Cart } from "../shop/components"
+import { getProducts, Product } from "../shop/shopify"
+
+type ShopProps = {
+  products: Product[]
+}
+
+export const getStaticProps: GetStaticProps<ShopProps> = async (context) => {
+  return {
+    props: {
+      products: await getProducts(),
+    },
+  }
+}
+
+export default function Shop({ products }: ShopProps) {
+  return (
+    <>
+      <Header />
+      <Cart product={products[0]} />
+    </>
+  )
 }
