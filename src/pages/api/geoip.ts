8ace6fa99a8d38b498d5004d847e5def50d5fec9
Alex Turpin <alex.turpin@gmail.com>
2021-05-22 18:32:42 -0400

    Debug GeoIP

--- src/pages/api/geoip.ts
+++ src/pages/api/geoip.ts
@@ -1,16 +1,12 @@
 import type { NextApiRequest, NextApiResponse } from "next"
 import maxmind, { CityResponse } from "maxmind"
 
-type Data = {
-  city: string | undefined
-}
-
-const geoip = async (req: NextApiRequest, res: NextApiResponse<Data | null>) => {
+const geoip = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "GET") return res.status(405).end()
 
   let ip = req.headers["X-Forwaded-For"] ?? process.env.FALLBACK_IP
   if (Array.isArray(ip)) ip = ip[0]
-  if (!ip) return res.status(400).end()
+  if (!ip) return res.status(400).send(req.headers)
 
   const lookup = await maxmind.open<CityResponse>("data/GeoLite2-City.mmdb")
   const response = lookup.get(ip)
