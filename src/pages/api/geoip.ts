298b8d30a41e793a24ef3d94e01770d4b31dc223
Alex Turpin <alex.turpin@gmail.com>
2021-05-22 18:36:13 -0400

    Fix GeoIP

--- src/pages/api/geoip.ts
+++ src/pages/api/geoip.ts
@@ -4,9 +4,9 @@ import maxmind, { CityResponse } from "maxmind"
 const geoip = async (req: NextApiRequest, res: NextApiResponse) => {
   if (req.method !== "GET") return res.status(405).end()
 
-  let ip = req.headers["X-Forwaded-For"] ?? process.env.FALLBACK_IP
+  let ip = req.headers["x-real-ip"] ?? process.env.FALLBACK_IP
   if (Array.isArray(ip)) ip = ip[0]
-  if (!ip) return res.status(400).send(req.headers)
+  if (!ip) return res.status(400).end()
 
   const lookup = await maxmind.open<CityResponse>("data/GeoLite2-City.mmdb")
   const response = lookup.get(ip)
