67cb716f94d928d32d07f295c1a332e4f54bdfc1
Alex Turpin <alex.turpin@gmail.com>
2021-05-25 17:08:34 -0400

    Fix currency format for iOS

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,8 +1,10 @@
 export const formatCurrency = (value: number, locale: string) =>
-  value.toLocaleString(locale, {
-    style: "currency",
-    currency: "CAD",
-    currencyDisplay: "narrowSymbol",
-  })
+  value
+    .toLocaleString(locale, {
+      style: "currency",
+      currency: "CAD",
+      currencyDisplay: "symbol",
+    })
+    .replace(/CAD?/, "") // currencyDisplay: "narrowSymbol" not supported by iOS Safari
 
 export type Locale = "en-CA" | "fr-CA"
