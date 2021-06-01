9c6a36df973aedd532f0674c5f9dd0cac85c9ef2
Alex Turpin <alex.turpin@gmail.com>
2021-05-31 21:34:00 -0400

    Style locale switcher

--- src/components/LocaleSwitcher.tsx
+++ src/components/LocaleSwitcher.tsx
@@ -5,7 +5,7 @@ export const LocaleSwitcher = () => {
   const { i18n } = useTranslation()
 
   return (
-    <span className="absolute top-2 right-2 w-10 py-3 z-50 text-center font-semibold rounded-md text-xs text-white uppercase bg-gradient-to-r from-orange to-mangenta tracking-widest">
+    <span className="absolute top-2 right-2 w-10 py-3 z-50 text-center font-semibold rounded-md text-xs text-white uppercase bg-purple-dark tracking-widest">
       {i18n.language === "en-CA" && (
         <Link href="/" locale="fr-CA">
           <a>FR</a>
